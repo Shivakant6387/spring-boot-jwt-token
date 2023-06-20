515b3ecac358ae60b25d779bbccb7cf8f8e507c0
Shivakant6387 <shivakant5875@gmail.com>
2023-06-20 14:18:52 +0530

    added jwt token dependence

---
+++ src/main/java/com/example/springbootjwttoken/SpringBootJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjwttoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtTokenApplication.class, args);
+	}
+
+}
